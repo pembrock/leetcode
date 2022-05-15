89830f57647a118fd8084c4f743a901bea5e54fb
Kirill Pasikuta <pembrock@gmail.com>
2022-05-15 20:50:22 +0300

    20. Valid Parentheses

---
+++ php/easy/20-valid-parentheses.php
@@ -0,0 +1,39 @@
+<?php
+/** https://leetcode.com/problems/valid-parentheses/ */
+
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Boolean
+     */
+    function isValid($s) {
+        if (mb_strlen($s) % 2 != 0) {
+            return false;
+        }
+
+        $brackets = [
+            '(' => ')',
+            '[' => ']',
+            '{' => '}'
+        ];
+        $bracketsArray = mb_str_split($s);
+        $bracketsStack = [];
+        foreach ($bracketsArray as $key => $value) {
+            if (isset($brackets[$value])) {
+                $bracketsStack[] = $value;
+            } else {
+                $last = array_pop($bracketsStack);
+                if ($brackets[$last] != $value) {
+                    return false;
+                }
+            }
+        }
+
+        return empty($bracketsStack);
+    }
+}
+
+$solution = new Solution();
+$s = "([{}])";
+var_dump($solution->isValid($s));
